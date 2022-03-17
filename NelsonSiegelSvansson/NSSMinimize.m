b1e6339b69649bb2d455825e6687b5feaf7bc9ce
Gregor <38689034+GregorFabjan@users.noreply.github.com>
2022-03-16 17:37:01 -0700

    First version of the NSS algorithm written in Matlab

---
+++ NelsonSiegelSvansson/NSSMinimize.m
@@ -0,0 +1,27 @@
+function out = NSSMinimize(beta0, beta1, beta2, beta3, lambda0, lambda1, TimeVec, YieldVec)
+    % NSSMinimize uses the built-in Matlab function "fminsearch". The Euclidian norm is used as a measure of distance between the forcasted numbers and the observed data.
+    % If the optimization does not converge, the function returns an empty vector.  
+    %
+    % Arguments:
+    %    beta0 =    1 x 1 floating number, representing the first factor of the NSS parametrisation  
+    %    beta1 =    1 x 1 floating number, representing the second factor of the NSS parametrisation 
+    %    beta2 =    1 x 1 floating number, representing the third factor of the NSS parametrisation
+    %    beta3 =    1 x 1 floating number, representing the fourth factor of the NSS parametrisation
+    %    lambda0 =  1 x 1 floating number, representing the first shape parameter lambda of the NSS parametrisation
+    %    lambda1 =  1 x 1 floating number, representing the second shape parameter lambda of the NSS parametrisation 
+    %    TimeVec =  n x 1 vector of maturities for which the yields in YieldVec were observed
+    %    YieldVec = n x 1 vector of observed yields 
+    % 
+    % Returns:
+    %     6 x 1 vector of parameters and factors, that best fit the observed yields (or an empty array if the optimization was not successfull).
+    %
+    % LINK TO SOURCE
+    fun = @(params)sum((NelsonSiegelSvansson(TimeVec, params(1), params(2), params(3), params(4), params(5), params(6))-YieldVec).^2); 
+    [opt_sol,~,isSuccess] = fminsearch(fun, [beta0; beta1; beta2; beta3; lambda0; lambda1]);
+    if (isSuccess)
+        out = opt_sol;
+    else
+        out = [];
+    end
+end
+
---
+++ NelsonSiegelSvansson/NelsonSiegelSvansson.m
@@ -0,0 +1,24 @@
+function out = NelsonSiegelSvansson(T, beta0, beta1, beta2, beta3, lambda0, lambda1)
+    % NelsonSiegelSvansson calculates the interpolated/extrapolated curve at points in the array "T" using the Nelson-Siegel-Svannson algorithm,
+    % parameterized with parameters beta0, beta1, beta2, beta3, lambda0, and lambda1. It returns a vector of points.
+    % 
+    % Arguments:
+    %    T =       n x 1 vector of maturities for which the user wants to calculate the coresponding rate
+    %    beta0 =   1 x 1 floating number, representing the first factor of the NSS parametrisation  
+    %    beta1 =   1 x 1 floating number, representing the second factor of the NSS parametrisation 
+    %    beta2 =   1 x 1 floating number, representing the third factor of the NSS parametrisation
+    %    beta3 =   1 x 1 floating number, representing the fourth factor of the NSS parametrisation
+    %    lambda0 = 1 x 1 floating number, representing the first shape parameter lambda of the NSS parametrisation
+    %    lambda1 = 1 x 1 floating number, representing the second shape parameter lambda of the NSS parametrisation 
+    % 
+    % Returns:
+    %     n x 1 vector of interpolated/extrapolarted points coresponding to maturities inside T. Where n is the length of the vector T.
+    % 
+    %     LINK TO SOURCE
+    alpha1 = (1 - exp(-T ./ lambda0)) ./ (T ./ lambda0);
+    alpha2 = alpha1 - exp(-T ./ lambda0);
+    alpha3 = (1 - exp(-T ./ lambda1)) ./ (T ./ lambda1) - exp(-T ./ lambda1);
+
+    out = beta0 + beta1*alpha1 + beta2*alpha2 + beta3*alpha3;
+end
+
