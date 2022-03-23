d6b09e63b3ad47286757018f08306b715ca53f29
Gregor <38689034+GregorFabjan@users.noreply.github.com>
2022-03-23 16:13:40 -0700

    Removed commented code
    Does not serve the main purpose which is to illustrate how to produce correlated Brownian motions

--- correlated_brownian_motion/CorrBM.m
+++ correlated_brownian_motion/CorrBM.m
@@ -1,8 +1,4 @@
-%E = [4,12,-16; 12,37,-43;-16,-43,98];
-%Cholesky(E)
 mu = [1;0];
-VarCovar = [1.5,0.8; 0.8,2];
+E = [1.5,0.8; 0.8,2];
 sampleSize = 5;
-CorBrownian(mu, VarCovar, sampleSize)
-
-%VarCovar = np.matrix('1.5, 0.8; 0.8, 2')
+CorBrownian(mu, E, sampleSize)
